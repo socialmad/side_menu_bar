a504a1b560b9d907ae1bade112f725833d19fb08
MD Sarfaraj <70369628+socialmad@users.noreply.github.com>
2022-08-08 10:37:41 +0530

    Update slide_bar_menu.dart

--- lib/src/slide_bar_menu.dart
+++ lib/src/slide_bar_menu.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:side_menu_bar/src/side_bar_menu_item.dart';
 
 class SideMenuBar extends StatefulWidget {
-  final List<SideBarMenuItem> sideMenuItems;
+  final List<Widget> sideMenuItems;
   final Color? backgroundColor;
 
   SideMenuBar({required this.sideMenuItems, this.backgroundColor});
